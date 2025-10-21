83c401b69b29925b75b4f04e6310a3898e3e9702
paintbeat <40527743+paintbeat@users.noreply.github.com>
2025-10-21 11:17:19 -0500

    Update start_optcg.sh

--- start_optcg.sh
+++ start_optcg.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Launch virtual X display
-Xvfb :1 -screen 0 1280x720x16 &
-sleep 2
-export DISPLAY=:1
-# Run OPTCG Sim
-wine "/opt/optcg/OPTCG-Sim.exe"
+cd /opt/optcg
+./OPTCGSim.x86_64
